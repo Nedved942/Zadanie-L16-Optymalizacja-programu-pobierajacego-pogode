9450a9e851c9584d6eaf8b1afc5803f109ca458b
nedved942 <pawelkoziol77@wp.pl>
2023-10-25 23:13:15 +0200

    Wykonanie pokodu spełniającego podstawową fukcję

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 from datetime import date, timedelta
 from json import dumps, loads, JSONDecodeError
 
-print("*** Obsługa biblioteki zewnętrznej ***")
+print("*** Obsługa biblioteki zewnętrznej - wersja zoptymalizowana ***")
 
 
 class WeatherForecast:
@@ -21,88 +21,86 @@ class WeatherForecast:
     def items(self):
         return ((key, value) for key, value in self.weather_data.items())
 
-    def read_file(self, path_file):
-        try:
-            with open(path_file) as file_stream:
-                self.file = file_stream.read()
-
-            if not self.file:
-                self.file = {}
-            else:
-                self.file = loads(self.file)
-        except FileNotFoundError:
-            print("Nie znaleziono pliku z historią.")
-        except JSONDecodeError as e:
-            print(f"Wystąpił nieoczekiwany błąd {e}.")
-
-# TODO Zweryfikuj przydatność tych zapisów, szczególnie z try-except
-
-    def write_file(self, path_file, file):
+    def write_file(self, path_file):
         with open(path_file, "w") as file_stream:
-            file_stream.write(dumps(file))
+            file_stream.write(dumps(self.weather_data))
 
-    def request_to_API(self, latitude, longtitude):
-        latitude = "51.248258"  # Szerokość geograficzna
-        longitude = "22.535016"  # Długość geograficzna
-        URL = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude" \
+    def request_to_api(self, latitude, longitude, searched_date):
+        url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude" \
               f"={longitude}&hourly=rain&daily=rain_sum&timezone=Europe%2FLondon&" \
               f"start_date={searched_date}&end_date={searched_date}"
-        data = requests.get(URL)
+        data = requests.get(url)
         data_dict = data.json()
+        try:
+            return data_dict['daily']['rain_sum'][0]
+        except KeyError:
+            print("Uwaga: Nie ma podanej wartości opadów dla danego dnia.")
+            return None
+
+
+def read_file(path_file):
+    try:
+        with open(path_file) as file_stream:
+            history_weather_forecast = file_stream.read()
+
+        if not history_weather_forecast:
+            return {}
+        else:
+            return loads(history_weather_forecast)
+    except FileNotFoundError:
+        print("Nie znaleziono pliku z historią.")
+    except JSONDecodeError as e:
+        print(f"Wystąpił nieoczekiwany błąd {e}.")
+
+
+def return_correct_date(date_to_check):
+    try:
+        date_to_check = date.fromisoformat(date_to_check)
+        date_to_check = str(date_to_check)
+    except ValueError:
+        date_to_check = None
+
+    if date_to_check:
+        return date_to_check
+    else:
+        tomorrow_day = str(date.today() + timedelta(days=1))
+        print(f"Jako datę przyjęto jutrzejszy dzień: {tomorrow_day}")
+        return tomorrow_day
+
+
+def is_it_rain(sum_of_rain):
+    if sum_of_rain is None:
+        print("Nie wiadomo czy będzie padać.")
+    elif sum_of_rain > 0:
+        print("Będzie padać.")
+    elif sum_of_rain == 0:
+        print("Nie będzie padać.")
 
 
 day_from_user = input("Podaj dzień, dla którego chcesz sprawdzić pogodę (YYYY-mm-dd): ")
+searched_date = return_correct_date(day_from_user)
+latitude = "51.248258"  # Szerokość geograficzna
+longitude = "22.535016"  # Długość geograficzna
+path_history_file = "history_weather_forecast.json"
 
-try:
-    day_from_user = date.fromisoformat(day_from_user)
-    day_from_user = str(day_from_user)
-except ValueError:
-    day_from_user = None
+weather_forecast = WeatherForecast(read_file(path_history_file))
 
-if day_from_user:
-    searched_date = day_from_user
-else:
-    searched_date = str(date.today() + timedelta(days=1))
-    print(f"Jako datę przyjęto jutrzejszy dzień: {searched_date}")
-
-weather_forecast = WeatherForecast(searched_date)
-
-
-
-
-
-
-
-
-
-# print(weather_forecast[date])
-# print(next(weather_forecast.items()))
+# print(weather_forecast.weather_data)
+# print(weather_forecast["2023-10-24"])
+# weather_forecast["2023-10-24"] = 13.7
+# print(weather_forecast["2023-10-24"])
+# weather_data_generator = weather_forecast.items()
+# print(next(weather_data_generator))
+# print(next(weather_data_generator))
+#
 # for item in weather_forecast:
 #     print(item)
 
+if searched_date in weather_forecast:
+    rain_sum = weather_forecast[searched_date]
+else:
+    rain_sum = weather_forecast.request_to_api(latitude, longitude, searched_date)
+    weather_forecast[searched_date] = rain_sum
+    weather_forecast.write_file(path_history_file)
 
-#
-# if searched_date in history_weather_forecast:
-#     rain_sum = history_weather_forecast[searched_date]
-# else:
-#
-#
-#     data_dict = data.json()
-#     try:
-#         rain_sum = data_dict['daily']['rain_sum'][0]
-#     except KeyError:
-#         rain_sum = None
-#
-#     if rain_sum is None:
-#         print("Nie wiem")
-#         exit()
-#
-#     history_weather_forecast[searched_date] = rain_sum
-#
-# print(f"Data prognozy pogody: {searched_date}.")
-# if rain_sum > 0:
-#     print("Będzie padać.")
-# elif rain_sum == 0:
-#     print("Nie będzie padać.")
-# else:
-#     print("Nie wiem.")
+is_it_rain(rain_sum)
